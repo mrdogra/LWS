a4e5038a6ac1ced90aabda9fd7e4b49c51f7d013
Achleshwar Dogra <92023431+mrdogra@users.noreply.github.com>
2021-12-10 18:11:50 +0530

    Update posts.blade.php

--- posts.blade.php
+++ posts.blade.php
@@ -1,31 +1,21 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/app.css">
+@extends('layout')
 
-    <title>Laravel - Blogs</title>
+@section('content')
 
-</head>
-<body>
+    @foreach($posts as $post)
 
-          <?php foreach ($posts as $post) : ?>
+    <article>
+        <h1>
+            <a href="/posts/{{$post->slug}}">
+                {{$post->title}}
+            </a>
+        </h1>
 
-           <article>
-              <h1>
-                  <a href="/posts/<?= $post->slug; ?>">
-                      <?= $post->title; ?>
-                  </a>
-              </h1>
+        <div>
+            {{$post->excerpt}}
+        </div>
+    </article>
 
-               <div>
-                   <?=$post->excerpt; ?>
-               </div>
-           </article>
+    @endforeach
 
-          <?php endforeach; ?>
-
-</body>
-
-</html>
+@endsection
